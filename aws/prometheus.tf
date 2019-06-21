81fb88dc5830674b439501fa10b4b077a42b6117
Morl99 <choffmann87@gmail.com>
2019-06-21 13:33:07 +0200

    Added first terraform to describe an IAM user that can be used by prometheus to view a list of ec2 instances. Also used as a hello world for my local terraform setup.

---
+++ aws/prometheus.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "C:/Users/test/.aws/credentials"
+  profile                 = "default"
+}
+
+resource "aws_iam_user" "prometheus" {
+  name = "prometheus"
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "prometheus" {
+  user = "${aws_iam_user.prometheus.name}"
+}
+
+resource "aws_iam_user_policy" "prometheus_describe" {
+  name = "ec2_describe"
+  user = "${aws_iam_user.prometheus.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
